e3975f127c03c45d94202df6429992d2215e9805
Miroslav Balev <m.balev@editcreation.com>
2023-12-28 20:34:44 +0200

    fix roles seeder

--- database/seeders/RolesSeeder.php
+++ database/seeders/RolesSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use App\Models\Role;
 
 class RolesSeeder extends Seeder
@@ -13,7 +14,9 @@ class RolesSeeder extends Seeder
      */
     public function run(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Role::truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         Role::updateOrCreate(['name' => 'Admin'],[
 			'name' => 'Admin'
