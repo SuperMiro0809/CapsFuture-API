f16cdaa919214cf6fd886a357e5bdf6c6541ad78
Miroslav Balev <m.balev@editcreation.com>
2024-01-02 21:46:59 +0200

    store campaign

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\{
     Campaign,
-    CampaignAttendance
+    CampaignAttendance,
+    CampaignCity,
+    Translation
 };
 
 class CampaignController extends Controller
@@ -17,7 +19,33 @@ class CampaignController extends Controller
 
     public function store(Request $request)
     {
-        //
+        $cities = json_decode($request->cities, true);
+        $information = json_decode($request->information, true);
+
+        $campaign = Campaign::create([
+            'title_image_path' => 'test',
+            'date' => $request->date
+        ]);
+
+        foreach($cities as $city) {
+            CampaignCity::create([
+                'campaign_id' => $campaign->id,
+                'city' => $city
+            ]);
+        }
+
+        foreach($information as $key=>$info) {
+            Translation::create([
+                'parent_id' => $campaign->id,
+                'model' => Campaign::class,
+                'title' => $info['title'],
+                'short_description' => $info['short_description'],
+                'description' => $info['description'],
+                'language' => $key
+            ]);
+        }
+
+        return $campaign;
     }
 
     public function update(Request $request, $id)
