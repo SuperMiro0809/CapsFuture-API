269b5b878229bb03956dc1c148649ff972916b21
Miroslav Balev <m.balev@editcreation.com>
2024-01-02 22:13:37 +0200

    db transaction for campaign store

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Models\{
     Campaign,
     CampaignAttendance,
@@ -22,30 +23,34 @@ class CampaignController extends Controller
         $cities = json_decode($request->cities, true);
         $information = json_decode($request->information, true);
 
-        $campaign = Campaign::create([
-            'title_image_path' => 'test',
-            'date' => $request->date
-        ]);
-
-        foreach($cities as $city) {
-            CampaignCity::create([
-                'campaign_id' => $campaign->id,
-                'city' => $city
+        $result = DB::transaction(function () use ($request, $cities, $information) {
+            $campaign = Campaign::create([
+                'title_image_path' => 'test',
+                'date' => $request->date
             ]);
-        }
 
-        foreach($information as $key=>$info) {
-            Translation::create([
-                'parent_id' => $campaign->id,
-                'model' => Campaign::class,
-                'title' => $info['title'],
-                'short_description' => $info['short_description'],
-                'description' => $info['description'],
-                'language' => $key
-            ]);
-        }
+            foreach($cities as $city) {
+                CampaignCity::create([
+                    'campaign_id' => $campaign->id,
+                    'city' => $city
+                ]);
+            }
 
-        return $campaign;
+            foreach($information as $key=>$info) {
+                Translation::create([
+                    'parent_id' => $campaign->id,
+                    'model' => Campaign::class,
+                    'title' => $info['title'],
+                    'short_description' => $info['short_description'],
+                    'description' => $info['description'],
+                    'language' => $key
+                ]);
+            }
+
+            return $campaign;
+        });
+
+        return $result;
     }
 
     public function update(Request $request, $id)
